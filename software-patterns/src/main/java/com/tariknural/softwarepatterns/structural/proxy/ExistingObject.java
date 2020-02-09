66daf8fe1db7aaabbf5d8bfcf040ac931fbfc569
Tarik <tariknural00@gmail.com>
2020-02-09 14:48:21 +0100

    update

--- software-patterns/src/main/java/com/tariknural/softwarepatterns/structural/proxy/ExistingObject.java
+++ software-patterns/src/main/java/com/tariknural/softwarepatterns/structural/proxy/ExistingObject.java
@@ -1,12 +1,10 @@
 package com.tariknural.softwarepatterns.structural.proxy;
 
-public class ExistingObject implements CommonInterface{
+public class ExistingObject implements CommonInterface {
 
     @Override
     public int returnNumber() {
         return 5;
     }
-
-
 
 }
